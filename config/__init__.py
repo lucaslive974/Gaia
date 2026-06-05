144b03847f0b4bbd8541fad7a0a7b75277f656e8
Lucas-Ribeiro-Lima <lucasribeirolima974@gmail.com>
2026-06-05 19:45:59 -0300

    refactor(config): Add a setting class to parse cmd arguments.

--- config/__init__.py
+++ config/__init__.py
@@ -1,1 +1,3 @@
-from config.settings import settings
+from config.settings import Settings
+
+settings = Settings()
--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,34 @@
+from argparse import Namespace
 import os
 
-settings = {
-    "BASE_PATH": "",
-    "TRAINED_DATA_DIR": os.path.join(os.getcwd(), "traineddata"),
-    "OUTPUT_CSV": os.path.join(os.getcwd(), "output.csv"),
-}
+input_attr = [
+    ["input_dir", "BASE_PATH"],
+    ["output", "OUTPUT_CSV"],
+    ["trainned_data", "TRAINED_DATA_DIR"],
+    ["mode", "MODE"],
+]
+
+
+class Settings:
+    BASE_PATH: str = ""
+    TRAINED_DATA_DIR: str = os.path.join(os.getcwd(), "traineddata")
+    OUTPUT_CSV: str = os.path.join(os.getcwd(), "output.csv")
+    MODE: str = "native"
+
+    def __getitem__(self, attr):
+        try:
+            val = getattr(self, attr)
+            return val
+        except AttributeError:
+            raise KeyError
+
+    def __setitem__(self, attr, value):
+        setattr(self, attr, value)
+
+    def __contains__(self, attr):
+        return hasattr(self, attr)
+
+    def parse_cmd_args(self, args: Namespace):
+        for attr in input_attr:
+            if hasattr(args, attr[0]):
+                setattr(self, attr[1], getattr(args, attr[0]))
